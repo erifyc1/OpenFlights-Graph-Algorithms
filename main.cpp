978cd338c10ef37a9ee29c6fa047674654010327
root <Jacob Stolker>
2022-04-16 05:29:39 +0000

    moved csv, txt to data folder, changed writeToFile

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ using namespace std;
 int main(int argc, char** argv) {
     // import data
     if (argc == 2 && argv[1][0] == 'c' && argv[1][1] == 's' && argv[1][2] == 'v') {
-        const string s = "routes.csv";
+        const string s = "./data/routes.csv";
         DataHandler d(s, true);
         cout << "constructed map" << endl;
-        d.writeMapToFile("compressed.txt");
+        d.writeMapToFile("./data/compressed.txt");
         cout << "done" << endl;
     }
     else {
